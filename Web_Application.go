9a7f1bff80bcfe56d2771a04597f47f6a451f7ad
Cian Gannon <lego@live.ie>
2017-10-26 22:23:04 +0100

    Question 8

--- Web_Application.go
+++ Web_Application.go
@@ -1,14 +1,60 @@
 package main
 
 import (
-	"log"
+	"html/template"
 	"net/http"
+	"math/rand"
+	"time"
+	"strconv"
 )
+	
+type myMsg struct {
+	Output string
+	YourGuess int
+}
+
+func guessHandler(w http.ResponseWriter, r *http.Request) {
+
+	output :="initialise"
+	
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	randNum:=rand.Intn(20-1)
+
+	var cookie, err = r.Cookie("randNum")
+
+	if err == nil{
+		randNum, _ = strconv.Atoi(cookie.Value)
+	}
+
+	yourGuess,_ := strconv.Atoi(r.FormValue("guess"))
+
+	if yourGuess== randNum{
+		output ="You guessed correctly!"
+	}else if yourGuess == 0{
+		output ="Input your guess from 1 - 20"
+	}else if yourGuess > 20{
+		output ="Input too high, please guess from 1 - 20"
+	}else if yourGuess < 0{
+		output ="Input too low, please guess from 1 - 20"
+	}else if yourGuess < randNum{
+		output="Incorrect Try Again. (Guessed too low)"
+	}else {
+		output="Incorrect Try Again. (Guessed too high)"
+	}
+
+	cookie = &http.Cookie{ Name: "randNum",Value: strconv.Itoa(randNum), Expires: time.Now().Add(72 * time.Hour)}
+	
+	http.SetCookie(w,cookie)
+	
+	t, _ := template.ParseFiles("guess.tmpl")
+	
+	t.Execute(w, &myMsg{Output:output})
+}
 
 func main() {
-	// Handles HTTP requests
-	http.Handle("/", http.FileServer(http.Dir("./guess")))
+	
+	http.HandleFunc("/", guessHandler)
 
-	// Displays Errors to the console
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.ListenAndServe(":8080", nil)
 }
