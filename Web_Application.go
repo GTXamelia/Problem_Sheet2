71e6ee5a6c8b43d059900d5086eaacc77ee45d4e
Cian Gannon <lego@live.ie>
2017-10-27 15:41:56 +0100

    Question 8 update

--- Web_Application.go
+++ Web_Application.go
@@ -10,7 +10,7 @@ import (
 	
 type myMsg struct {
 	Output string
-	YourGuess int
+	UserGuess int
 }
 
 func guessHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,38 +18,36 @@ func guessHandler(w http.ResponseWriter, r *http.Request) {
 	output :="initialise"
 	
 	rand.Seed(time.Now().UTC().UnixNano())
-
 	randNum:=rand.Intn(20-1)
 
+	guessTemplate, _ := template.ParseFiles("guess.tmpl")
+
 	var cookie, err = r.Cookie("randNum")
+
+	userGuess,_ := strconv.Atoi(r.FormValue("guess"))
 
 	if err == nil{
 		randNum, _ = strconv.Atoi(cookie.Value)
 	}
 
-	yourGuess,_ := strconv.Atoi(r.FormValue("guess"))
+	cookie = &http.Cookie{ Name: "randNum",Value: strconv.Itoa(randNum), Expires: time.Now().Add(72 * time.Hour)}
+	http.SetCookie(w,cookie)
 
-	if yourGuess== randNum{
+	if userGuess== randNum{
 		output ="You guessed correctly!"
-	}else if yourGuess == 0{
+	}else if userGuess == 0{
 		output ="Input your guess from 1 - 20"
-	}else if yourGuess > 20{
+	}else if userGuess > 20{
 		output ="Input too high, please guess from 1 - 20"
-	}else if yourGuess < 0{
+	}else if userGuess < 0{
 		output ="Input too low, please guess from 1 - 20"
-	}else if yourGuess < randNum{
+	}else if userGuess < randNum{
 		output="Incorrect Try Again. (Guessed too low)"
 	}else {
 		output="Incorrect Try Again. (Guessed too high)"
 	}
-
-	cookie = &http.Cookie{ Name: "randNum",Value: strconv.Itoa(randNum), Expires: time.Now().Add(72 * time.Hour)}
 	
-	http.SetCookie(w,cookie)
-	
-	t, _ := template.ParseFiles("guess.tmpl")
-	
-	t.Execute(w, &myMsg{Output:output})
+	guessTemplate.Execute(w, &myMsg{Output:output,UserGuess:userGuess})
 }
 
 func main() {
